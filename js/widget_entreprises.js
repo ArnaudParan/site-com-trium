0a5936d8b8a0bd34e8b3dd21b6496c07fcc72cd2
arnaud_paran <paran.arnaud@gmail.com>
2015-04-30 11:34:35 +0200

    positionnement du widget entreprises dans le javascript, il s'adapte à sa largeur

--- js/widget_entreprises.js
+++ js/widget_entreprises.js
@@ -10,6 +10,7 @@ var taille_media=lg;
 $(function(){
 	$(window).resize(redimensionnement);
 	$('.ent').hover(hov,dehov);
+	positionne_widget(500);
 });
 
 //fonction appelée au redimensionnement, qui détermine si on a changé de catégorie de média
@@ -43,9 +44,9 @@ function chg_cat(cat){
 
 //fonction lancée quand on met le curseur sur un des boutons du widget
 function hov(){
+	var l_sbtn=parseInt($('#sbtn11').css('width'));
 	if($(this).attr("anim")!="true" && $(this).attr("anim")!="debutfalse"){
 		$(this).attr("anim","true");
-		console.log($(this).attr("anim"));
 		var id=-1;
 		if($(this).attr("id")=="btn1"){
 			id=1;
@@ -66,14 +67,16 @@ function hov(){
 			id=6;
 		}
 		$(this).animate({opacity:0.},200,function(){
-			$('#sbtn'+id+'1').animate({top:"-=60"},500);
-			$('#sbtn'+id+'2').animate({top:"+=35",left:"+=55"},500);
-		$('#sbtn'+id+'3').animate({top:"+=35",left:"-=55"},500);
+			$('#sbtn'+id+'1').animate({top:"-="+(Math.sqrt(3)/2-1/(2*Math.sqrt(3)))*l_sbtn},500);
+			$('#sbtn'+id+'2').animate({top:"+="+(Math.sqrt(3)/2-2/(2*Math.sqrt(3)))*l_sbtn,left:"+="+l_sbtn/2},500);
+			$('#sbtn'+id+'3').animate({top:"+="+(Math.sqrt(3)/2-2/(2*Math.sqrt(3)))*l_sbtn,left:"-="+l_sbtn/2},500);
 		});
 	}
 }
 
 function dehov(){
+	var l_btn=parseInt($('#btn1').css('width'));
+	var l_sbtn=parseInt($('#sbtn11').css('width'))
 	if($(this).attr("anim")=="true"){
 		$(this).attr("anim","debutfalse");
 		var id=-1;
@@ -95,13 +98,39 @@ function dehov(){
 		else if($(this).attr("id")=="btn6"){
 			id=6;
 		}
-		$('#sbtn'+id+'1').animate({top:"+=60"},500);
-		$('#sbtn'+id+'2').animate({top:"-=35",left:"-=55"},500);
-		$('#sbtn'+id+'3').animate({top:"-=35",left:"+=55"},500,function(){
+		$('#sbtn'+id+'1').animate({top:"+="+(Math.sqrt(3)/2-1/(2*Math.sqrt(3)))*l_sbtn},500);
+		$('#sbtn'+id+'2').animate({top:"-="+(Math.sqrt(3)/2-2/(2*Math.sqrt(3)))*l_sbtn,left:"-="+l_sbtn/2},500);
+		$('#sbtn'+id+'3').animate({top:"-="+(Math.sqrt(3)/2-2/(2*Math.sqrt(3)))*l_sbtn,left:"+="+l_sbtn/2},500,function(){
 			$("#btn"+id).animate({opacity:1.},500, function(){
-				console.log("fin");
 				$(this).attr("anim","false");
 			});
 		});
+	}
+}
+
+/*positionne le widget en fonction de la largeur l qui lui est allouée*/
+function positionne_widget(l){
+	var l_btn=parseInt($('#btn1').css('width'));
+	var l_sbtn=parseInt($('#sbtn11').css('width'))
+	var vraie_largeur=l-l_btn;
+	var decalage_sbtn=(l_btn-l_sbtn)/2;
+	$('#btn1').css({'left':vraie_largeur/2+'px','top':'0px'});
+	$('#btn2').css({'left':vraie_largeur+'px','top':vraie_largeur/(2*Math.sqrt(3))+'px'});
+	$('#btn3').css({'left':vraie_largeur+'px','top':3*vraie_largeur/(2*Math.sqrt(3))+'px'});
+	$('#btn4').css({'left':vraie_largeur/2+'px','top':4*vraie_largeur/(2*Math.sqrt(3))+'px'});
+	$('#btn5').css({'left':'0px','top':3*vraie_largeur/(2*Math.sqrt(3))+'px'});
+	$('#btn6').css({'left':'0px','top':vraie_largeur/(2*Math.sqrt(3))+'px'});
+
+	for(i=1;i<=6;i++){
+		for(j=1;j<=3;j++){
+			$('#sbtn'+i+''+j).css({'left':$('#btn'+i).css('left'),'top':$('#btn'+i).css('top')});
+			$('#sbtn'+i+''+j).css({'left':'+='+decalage_sbtn+'px','top':'+='+decalage_sbtn+'px'});
+		}
+	}
+	for(i=1;i<=6;i++){
+		$('#btn'+i).css({'visibility':'visible'});
+		for(j=1;j<=3;j++){
+			$('#sbtn'+i+''+j).css({'visibility':'visible'});
+		}
 	}
 }
