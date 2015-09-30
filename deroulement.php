daef9fd3bc3c3ae53bd54e96179ca7c85e6cddea
arnaud_paran <paran.arnaud@gmail.com>
2015-09-30 15:46:18 +0200

    correction bug deroulement

--- deroulement.php
+++ deroulement.php
@@ -2,25 +2,29 @@
 	<h2 style="text-align:center;margin-bottom:20px">
 		Déroulement du forum
 	</h2>
-	<div class="col-sm-offset-3 col-sm-6">
+	<div class="col-sm-offset-5 col-sm-1">
 		<div class="row">
 			<div style="text-align:center;margin-bottom:10px">
 				8h00
 			</div>
+		</div>
+	</div>
+	<div class="col-sm-offset-5 col-sm-6">
+		<div class="row">
 			<div class="row creneau">
-				<div class="container"><div class="col-sm-5 col-sm-offset-4"><span class="infobulle">Les petits déjeuners vous permettent de discuter dans un cadre détendu avec les différents acteurs des entreprises</span></div></div>
-				<div class="col-sm-6 heure">
+				<div class="container"><div class="col-sm-5 col-sm-offset-1"><span class="infobulle">Les petits déjeuners vous permettent de discuter dans un cadre détendu avec les différents acteurs des entreprises</span></div></div>
+				<div class="col-sm-1 heure">
 					9h30
 				</div>
 				<div class="col-sm-6 tache">
 					Petits Déjeuners
-					<!--<button class="btn btn-danger">Vinci</button>
-					<button class="btn btn-danger">ATkearney</button>-->
+					<button class="btn btn-danger">Vinci</button>
+					<button class="btn btn-danger">ATkearney</button>
 				</div>
 			</div>
 			<div class="row creneau">
-				<div class="container"><span class="infobulle">Blabla</span></div>
-				<div class="col-sm-6 heure">
+				<div class="container"><div class="col-sm-5 col-sm-offset-1"><span class="infobulle">Les petits déjeuners vous permettent de discuter dans un cadre détendu avec les différents acteurs des entreprises</span></div></div>
+				<div class="col-sm-1 heure">
 					10h30
 				</div>
 				<div class="col-sm-6 tache">
@@ -28,8 +32,8 @@
 				</div>
 			</div>
 			<div class="row creneau">
-				<div class="container"><span class="infobulle">Blabla</span></div>
-				<div class="col-sm-6 heure">
+				<div class="container"><div class="col-sm-5 col-sm-offset-1"><span class="infobulle">Les petits déjeuners vous permettent de discuter dans un cadre détendu avec les différents acteurs des entreprises</span></div></div>
+				<div class="col-sm-1 heure">
 					17h00
 				</div>
 				<div class="col-sm-6 tache">
--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 	<!-- Lea Header et ses widgets-->
 	<header class="row">
 		<div class="banniere_haut"></div>
-		<div class="col-xs-5 col-sm-2 col-md-2 col-lg-1">
+		<div class="col-xs-5 col-sm-1 col-md-1 col-lg-1">
 			<div class="col-md-6 col-xs-6"><a href="#"><img class="lang" src="/logos/french.png" alt="Français"/></a></div>
 			<div class="col-md-6 col-xs-6"><a href="eng/"><img class="lang" src="/logos/english.png" alt="Anglais"/></a></div>
 		</div>
@@ -9,9 +9,9 @@
 			<a href="/"><img class="img" src="/logos/logo_trium.png" alt="FORUM TRIUM"/></a>
 		</div>
 		<!-- Le nav -->
-		<div class="col-xs-offset-0 col-sm-offset-0 col-md-offset-0 col-lg-offset-2 col-xs-12 col-sm-12 col-sm-7 col-lg-7">
+		<div class="col-xs-offset-0 col-sm-offset-1 col-md-offset-0 col-lg-offset-2 col-xs-12 col-sm-12 col-md-8 col-lg-7">
 			<div class="row">
-				<div class="col-lg-offset-1" style="margin-top:20px">
+				<div class="col-lg-offset-1 col-sm-offset-4" style="margin-top:20px">
 					<a href="http://www.sirom.net/trium/site/index.php"><button type="button" class="btn btn-success" aria-label="Left Align">
 						<span class="glyphicon glyphicon-list-alt" aria-hidden="true"></span> Le site entreprises
 					</button></a>
