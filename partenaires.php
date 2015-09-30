9d5f6a37d6d7c0778a6ad72179e92744c5593ad2
arnaud_paran <paran.arnaud@gmail.com>
2015-09-30 18:00:40 +0200

    ajout de quelques partenariats

--- partenaires.php
+++ partenaires.php
@@ -3,9 +3,77 @@
 		<div class="row">
 			<div class="col-lg-6" style="border-right: 1px solid grey">
 				<h2 style="text-align:center">Presse</h2>
+				<div class="row">
+					<div class="col-sm-3">
+						<img src="" alt="L'Étudiant"/>
+					</div>
+					<div class="col-sm-9">
+						<h3>L'Étudiant</h3>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-sm-3 col-sm-push-9">
+						<img src="" alt="Studyrama"/>
+					</div>
+					<div class="col-sm-9 col-sm-pull-3">
+						<h3>Studyrama</h3>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-sm-3">
+						<img src="" alt="Journal des Grandes Écoles"/>
+					</div>
+					<div class="col-sm-9">
+						<h3>Journal des Grandes Écoles et Universités</h3>
+					</div>
+				</div>
 			</div>
 			<div class="col-lg-6" style="border-left: 1px solid grey">
-				<h2 style="text-align:center">Entreprises</h2>
+				<h2 style="text-align:center">Entreprises et Associations</h2>
+				<div class="row">
+					<div class="col-sm-3">
+						<img src="" alt="Intermines"/>
+					</div>
+					<div class="col-sm-9">
+						<h3>Intermines</h3>
+						Constituée en juin 1983, intermines est une association regroupant les anciens des écoles des Mines. Cette année, elle proposera des simulations d'entretiens d'embauche aux étudiants des quatre écoles participantes, ainsi qu'un atelier de correction de CV et lettres de motivation.
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-sm-3 col-sm-push-9">
+						<img src="" alt="iQuesta"/>
+					</div>
+					<div class="col-sm-9 col-sm-pull-3">
+						<h3>iQuesta</h3>
+						iQuesta sera présente le jour du Forum pour conseiller les jeunes diplômés dans le cadre de leur recherche de stage et d'emploi (correction de CV, lettres de motivation). Leur service de correction de CV peut se faire également via la CV-thèque qu’ils ont créé pour le forum. 
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-sm-3">
+						<img src="" alt="JobTeaser"/>
+					</div>
+					<div class="col-sm-9">
+						<h3>JobTeaser</h3>
+						Ils proposent également une CV-thèque aux étudiants afin qu'ils y déposent leurs CVs et que les entreprises participant au forum puissent les consulter. Une graphologue sera présente sur leur stand. Ils proposeront également des corrections et des impressions de CV, ainsi que la possibilité de prendre de bonnes photos pour mettre sur son CV.
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-sm-3 col-sm-push-9">
+						<img src="" alt="DogFinance"/>
+					</div>
+					<div class="col-sm-9 col-sm-pull-3">
+						<h3>DogFinance</h3>
+						DogFinance sera chargée d’animer un Atelier Réseaux Sociaux, permettant aux visiteurs de faire le point sur leur E-réputation et de les aider à utiliser les différents réseaux sociaux professionnels.
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-sm-3">
+						<img src="" alt="RATP"/>
+					</div>
+					<div class="col-sm-9">
+						<h3>RATP</h3>
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
