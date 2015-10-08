874964eff1ec5a3751de468ff5e6f26ab681e5a8
arnaud_paran <paran.arnaud@gmail.com>
2015-10-08 15:09:52 +0200

    correction redirection a_venir

--- a_venir.php
+++ a_venir.php
@@ -1,4 +1,4 @@
 <div class="row">
 <p style="font-size:40px;font-weight:bold">Contenu à venir</p>
-<p style="font-size:20px;">La page à laquelle vous voulez accéder n'existe pas encore. Nous faisons tout pour vous la délivrer dans les plus brefs délais. En attendant, vous êtes prié de retourner vers <a href="/index.html">l'Accueil</a></p>
+<p style="font-size:20px;">La page à laquelle vous voulez accéder n'existe pas encore. Nous faisons tout pour vous la délivrer dans les plus brefs délais. En attendant, vous êtes prié de retourner vers <a href="/accueil.html">l'Accueil</a></p>
 </div>
