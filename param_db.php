2768c8ceab84dfcc07ae14b7425f4d187a84a5f2
arnaud_paran <paran.arnaud@gmail.com>
2015-10-22 11:13:42 +0200

    minor changes on db config

--- param_db.php
+++ param_db.php
@@ -1,4 +1,8 @@
 <?php
+global $db_host;
+global $db_dbname;
+global $db_user;
+global $db_password;
 $db_host = 'forumtrivwcom.postgresql.db ';
 $db_dbname = 'forumtrivwcom';
 $db_user = 'forumtrivwcom';
